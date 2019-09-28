3d1798a8479fcc316cb32a0975375f3286d6628e
shahar <shaharporat2@gmail.com>
2019-09-28 12:02:47 +0300

    Saving tasks to local storage

--- js/web.js
+++ js/web.js
@@ -1,3 +1,17 @@
+
+
 function saveTask(){
-    
+    var taskDetail=document.getElementById('taskDetail').value;
+    var endDate=document.getElementById('endDate').value;
+    var endHour=document.getElementById('endHour').value;
+
+    var indexTask = localStorage.length;
+    var task = { 
+        'id' : indexTask,
+        'taskDetail' : taskDetail,
+        'endDate' : endDate,
+        'endHour' : endHour
+    }
+    localStorage.setItem(task.id, JSON.stringify(task));
+    indexTask++;
 }
